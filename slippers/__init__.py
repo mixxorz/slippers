98a1f8b9ab80ee088186fc93050c8c533595fbdb
Mitchel Cabuloy <mixxorz@gmail.com>
2021-07-11 21:57:46 +0800

    Fix initial issues

--- slippers/__init__.py
+++ slippers/__init__.py
@@ -1,1 +1,3 @@
-__version__ = '0.1.0'
+__version__ = "0.1.1"
+
+default_app_config = "slippers.apps.SlippersConfig"
--- slippers/apps.py
+++ slippers/apps.py
@@ -1,7 +1,8 @@
 from pathlib import Path, PosixPath
 
 from django.apps import AppConfig
-from django.template.loader import get_template, render_to_string
+from django.template.exceptions import TemplateDoesNotExist
+from django.template.loader import get_template
 from django.utils.autoreload import autoreload_started, file_changed
 
 import yaml
@@ -12,29 +13,38 @@ from slippers.templatetags.slippers import (
 )
 
 
+def register_tags():
+    """Register tags from components.yml"""
+    try:
+        template = get_template("components.yml")
+        components = yaml.safe_load(template.template.source)
+        register_inline_components(components.get("inline_components", {}))
+        register_block_components(components.get("block_components", {}))
+    except TemplateDoesNotExist:
+        pass
+
+
 def watch(sender, **kwargs):
     """Watch when component.yml changes"""
-    template = get_template("components.yml")
-    sender.extra_files.add(Path(template.origin.name))
+    try:
+        template = get_template("components.yml")
+        sender.extra_files.add(Path(template.origin.name))
+    except TemplateDoesNotExist:
+        pass
 
 
 def changed(sender, file_path: PosixPath, **kwargs):
     """Refresh tag registry when component.yml changes"""
     if file_path.name == "components.yml":
         print("components.yml changed. Updating component tags...")
-        components = yaml.safe_load(render_to_string("components.yml"))
-        register_inline_components(components["inline_components"])
-        register_block_components(components["block_components"])
+        register_tags()
 
 
 class SlippersConfig(AppConfig):
     name = "slippers"
 
     def ready(self):
-        # Register tags from components.yml
-        components = yaml.safe_load(render_to_string("components.yml"))
-        register_inline_components(components["inline_components"])
-        register_block_components(components["block_components"])
+        register_tags()
 
         autoreload_started.connect(watch)
         file_changed.connect(changed)
