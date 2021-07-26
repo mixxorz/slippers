fd9ba76c7f6018525d3d0aac55f1580f528a2600
Mitchel Cabuloy <mixxorz@gmail.com>
2021-07-26 20:01:05 +0800

    Refactor tests

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -1,127 +1,81 @@
-from textwrap import dedent
-
 from django.template import Context, Template, TemplateSyntaxError
 from django.test import TestCase, override_settings
 
 
 class ComponentTest(TestCase):
     def test_render_inline_component(self):
-        template = Template(
-            dedent(
-                """
-            {% load slippers %}
-
+        template = """
             {% avatar user="mixxorz" %}
-            """
-            )
-        )
+        """
 
-        expected = dedent(
-            """
+        expected = """
             <div>I am avatar for mixxorz</div>
-            """
-        )
+        """
 
-        self.assertHTMLEqual(expected, template.render(Context()))
+        self.assertHTMLEqual(expected, Template(template).render(Context()))
 
     def test_render_block_component(self):
-        template = Template(
-            dedent(
-                """
-            {% load slippers %}
-
+        template = """
             {% #button %}I am button{% /button %}
-            """
-            )
-        )
+        """
 
-        expected = dedent(
-            """
+        expected = """
             <button>I am button</button>
-            """
-        )
+        """
 
-        self.assertHTMLEqual(expected, template.render(Context()))
+        self.assertHTMLEqual(expected, Template(template).render(Context()))
 
     def test_render_without_children(self):
-        template = Template(
-            dedent(
-                """
-            {% load slippers %}
-
+        template = """
             {% icon_button icon="envelope" %}
             {% #icon_button icon="envelope" %}Submit{% /icon_button %}
-            """
-            )
-        )
+        """
 
-        expected = dedent(
-            """
+        expected = """
             <button class="icon-button envelope"></button>
             <button class="icon-button envelope">Submit</button>
-            """
-        )
+        """
 
-        self.assertHTMLEqual(expected, template.render(Context()))
+        self.assertHTMLEqual(expected, Template(template).render(Context()))
 
     def test_render_nested(self):
-        template = Template(
-            dedent(
-                """
-            {% load slippers %}
-
+        template = """
             {% #card heading="I am heading" %}
-                {% #button %}I am button{% /button %}
+            {% #button %}I am button{% /button %}
             {% /card %}
-            """
-            )
-        )
+        """
 
-        expected = dedent(
-            """
+        expected = """
             <div class="card">
                 <div class="card__header">I am heading</div>
                 <div class="card__body">
                     <button>I am button</button>
                 </div>
             </div>
-            """
-        )
+        """
 
-        self.assertHTMLEqual(expected, template.render(Context()))
+        self.assertHTMLEqual(expected, Template(template).render(Context()))
 
     def test_kwargs_with_filters(self):
-        template = Template(
-            dedent(
-                """
-            {% load slippers %}
-
+        template = """
             {% #card heading="I am heading"|upper %}
-                {% #button %}I am button{% /button %}
+            {% #button %}I am button{% /button %}
             {% /card %}
-            """
-            )
-        )
+        """
 
-        expected = dedent(
-            """
+        expected = """
             <div class="card">
                 <div class="card__header">I AM HEADING</div>
                 <div class="card__body">
                     <button>I am button</button>
                 </div>
             </div>
-            """
-        )
+        """
 
-        self.assertHTMLEqual(expected, template.render(Context()))
+        self.assertHTMLEqual(expected, Template(template).render(Context()))
 
     def test_render_as_variable(self):
-        template = Template(
-            dedent(
-                """
-            {% load slippers %}
-
+        template = """
             {% avatar user="mixxorz" as my_avatar %}
             {% #button as my_button %}I am button{% /button %}
 
@@ -130,23 +84,27 @@ class ComponentTest(TestCase):
                 {{ my_avatar }}
                 {{ my_button }}
             </div>
-            """
-            )
-        )
+        """
 
-        expected = dedent(
-            """
+        expected = """
             <div>
                 <div>I am avatar for mixxorz</div>
                 <button>I am button</button>
             </div>
-            """
-        )
+        """
 
-        self.assertHTMLEqual(expected, template.render(Context()))
+        self.assertHTMLEqual(expected, Template(template).render(Context()))
 
     def test_pass_boolean_flags(self):
-        self.fail()
+        template = """
+            {% #button disabled %}I am button{% /button %}
+        """
+
+        expected = """
+            <button disabled>I am button</button>
+            """
+
+        self.assertHTMLEqual(expected, Template(template).render(Context()))
 
 
 class AttrsTagTest(TestCase):
@@ -159,19 +117,13 @@ class AttrsTagTest(TestCase):
             }
         )
 
-        template = dedent(
-            """
-            {% load slippers %}
-
+        template = """
             <input {% attrs type id name %}>
-            """
-        )
+        """
 
-        expected = dedent(
-            """
+        expected = """
             <input type="text" id="the_id" name="the_name">
-            """
-        )
+        """
 
         self.assertHTMLEqual(expected, Template(template).render(context))
 
@@ -183,19 +135,13 @@ class AttrsTagTest(TestCase):
             }
         )
 
-        template = dedent(
-            """
-            {% load slippers %}
-
+        template = """
             <button {% attrs autofocus disabled %}>Click me</button>
-            """
-        )
+        """
 
-        expected = dedent(
-            """
+        expected = """
             <button disabled>Click me</button>
-            """
-        )
+        """
 
         self.assertHTMLEqual(expected, Template(template).render(context))
 
@@ -208,124 +154,84 @@ class AttrsTagTest(TestCase):
             }
         )
 
-        template = dedent(
-            """
-            {% load slippers %}
-
+        template = """
             <input {% attrs type=input_type id name %}>
-            """
-        )
+        """
 
-        expected = dedent(
-            """
+        expected = """
             <input type="text" id="the_id" name="the_name">
-            """
-        )
+        """
 
         self.assertHTMLEqual(expected, Template(template).render(context))
 
 
 class VarTagTest(TestCase):
     def test_basic(self):
-        context = Context()
-
-        template = dedent(
-            """
-            {% load slippers %}
-
+        template = """
             {% var foo="Hello, World!" %}
 
             <div>{{ foo }}</div>
-            """
-        )
+        """
 
-        expected = dedent(
-            """
+        expected = """
             <div>Hello, World!</div>
-            """
-        )
+        """
 
-        self.assertHTMLEqual(expected, Template(template).render(context))
+        self.assertHTMLEqual(expected, Template(template).render(Context()))
 
     def test_value_filter(self):
-        context = Context()
-
-        template = dedent(
-            """
-            {% load slippers %}
-
+        template = """
             {% var foo=foo|default:"Default value" %}
             {% var bar="Hello, World!"|upper %}
 
             <div>{{ foo }}</div>
             <div>{{ bar }}</div>
-            """
-        )
+        """
 
-        expected = dedent(
-            """
+        expected = """
             <div>Default value</div>
             <div>HELLO, WORLD!</div>
-            """
-        )
+        """
 
-        self.assertHTMLEqual(expected, Template(template).render(context))
+        self.assertHTMLEqual(expected, Template(template).render(Context()))
 
 
 class MatchFilterTest(TestCase):
     def test_basic(self):
         context = Context({"first": "outline", "second": "ghost", "third": "square"})
 
-        template = dedent(
-            """
-            {% load slippers %}
-
+        template = """
             <button class="{{ first|match:"outline:btn-outline,ghost:btn-ghost" }}">Click me</button>
             <button class="{{ second|match:"outline:btn-outline,ghost:btn-ghost" }}">Click me</button>
             <button class="{{ third|match:"outline:btn-outline,ghost:btn-ghost" }}">Click me</button>
-            """
-        )
+        """
 
-        expected = dedent(
-            """
+        expected = """
             <button class="btn-outline">Click me</button>
             <button class="btn-ghost">Click me</button>
             <button class="">Click me</button>
-            """
-        )
+        """
 
         self.assertHTMLEqual(expected, Template(template).render(context))
 
     @override_settings(DEBUG=True)
     def test_syntax_error(self):
-        context = Context()
-
-        template = dedent(
-            """
-            {% load slippers %}
-
+        template = """
             <button class="{{ "foo"|match:"outline:btn-outline,foo:bar:baz,,:apple,:orange" }}">Click me</button>
-            """
-        )
+        """
 
         with self.assertRaises(TemplateSyntaxError):
-            Template(template).render(context)
+            Template(template).render(Context())
 
     def test_ignore_spaces(self):
         context = Context({"variant": "ghost"})
 
-        template = dedent(
-            """
-            {% load slippers %}
-
+        template = """
             <button class="{{ variant|match:"outline:btn-outline, ghost:btn-ghost" }}">Click me</button>
-            """
-        )
+        """
 
-        expected = dedent(
-            """
+        expected = """
             <button class="btn-ghost">Click me</button>
-            """
-        )
+        """
 
         self.assertHTMLEqual(expected, Template(template).render(context))
