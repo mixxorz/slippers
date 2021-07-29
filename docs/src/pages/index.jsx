83c67c65477e2a2ca0c1107da8e751fd2861d2f6
Mitchel Cabuloy <mixxorz@gmail.com>
2021-07-29 14:11:07 +0800

    Add install instructions

--- docs/src/pages/index.jsx
+++ docs/src/pages/index.jsx
@@ -4,42 +4,9 @@ import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
-import HomepageFeatures from "../components/HomepageFeatures";
-
-function HomepageHeader() {
-    const { siteConfig } = useDocusaurusContext();
-    return (
-        <header className={clsx("hero hero--primary", styles.heroBanner)}>
-            <div className="container">
-                <img src="img/slippers.svg" alt="slippers" />
-                <p
-                    className={clsx(
-                        "hero__subtitle margin-top--md",
-                        styles.heroSubtitle
-                    )}
-                >
-                    {siteConfig.tagline}
-                </p>
-                <div className={styles.buttons}>
-                    <Link
-                        className="button button--primary button--lg margin-top--md"
-                        to="/docs/intro"
-                    >
-                        Quick start
-                    </Link>
-                    <Link
-                        className={clsx("button button--secondary button--lg margin-top--md", styles.secondaryButton)}
-                        to="/docs/intro"
-                    >
-                        Docs
-                    </Link>
-                </div>
-            </div>
-        </header>
-    );
-}
 
 export default function Home() {
+    const { siteConfig } = useDocusaurusContext();
     return (
         <Layout
             title={
@@ -47,9 +14,42 @@ export default function Home() {
             }
             description="Build reusable components in Django without writing a single line of Python."
         >
-            <HomepageHeader />
-            <main>
-                <HomepageFeatures />
+            <main className={clsx("hero hero--primary", styles.heroBanner)}>
+                <div className="container">
+                    <img src="img/slippers.svg" alt="slippers" />
+                    <p
+                        className={clsx(
+                            "hero__subtitle margin-top--md",
+                            styles.heroSubtitle
+                        )}
+                    >
+                        {siteConfig.tagline}
+                    </p>
+                    <div className={styles.buttons}>
+                        <Link
+                            className="button button--primary button--lg margin-top--md"
+                            to="/docs/intro"
+                        >
+                            Quick start
+                        </Link>
+                        <Link
+                            className={clsx(
+                                "button button--secondary button--lg margin-top--md",
+                                styles.secondaryButton
+                            )}
+                            to="/docs/intro"
+                        >
+                            Docs
+                        </Link>
+                    </div>
+                    <div className="row margin-top--lg">
+                        <div className="col col--4">
+                            <pre>
+                                <span>$</span> pip install slippers
+                            </pre>
+                        </div>
+                    </div>
+                </div>
             </main>
         </Layout>
     );
